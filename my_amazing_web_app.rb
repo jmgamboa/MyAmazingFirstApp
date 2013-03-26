61b01e376ecbad912daf9e360b55836141679190
Joaquin Gamboa <gamboanj@gmail.com>
2013-03-25 22:24:00 -0400

    Minor styling changes, added notice for unchecked boxes, and text box

--- my_amazing_web_app.rb
+++ my_amazing_web_app.rb
@@ -11,22 +11,33 @@ get '/' do
 end
 
 post '/submit' do
-  @message = "Someone thought your image was #{params[:answer]}"
-  
-  # Change these to match your Twilio account settings 
-  @account_sid = "ACf5043ffa0a4bbfccf4e9ac9859db2f35"
-  @auth_token = "7e18c038d8aab03ee080eb399423a2bc"
-  
-  # Set up a client to talk to the Twilio REST API
-  @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+
+  if params[:tc] == [1]
     
-  @account = @client.account
-  @sms = @account.sms.messages.create({
-    :from => '+13472526388', 
-    :to => '+19089303419',
-    :body => @message
-  })
+    @message = "#{firstname} drinks their coffee #{params[:answer]}"
+    
+    
+    @account_sid = "ACf5043ffa0a4bbfccf4e9ac9859db2f35"
+    @auth_token = "7e18c038d8aab03ee080eb399423a2bc"
   
-  flash[:notice] = "SMS sent: #{@message}"
-  redirect '/'
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+      
+    @account = @client.account
+    @sms = @account.sms.messages.create({
+      :from => '+13472526388', 
+      :to => '+19089303419',
+      :body => @message
+    })
+    
+    flash[:notice] = "SMS sent: #{@message}"
+    redirect '/'
+     
+  else
+
+    flash[:notice] = "Please check the box to confirm your capabilities"
+    redirect '/'
+
+  end
+
+   
 end
